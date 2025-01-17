d60b0e6c20f2af06dd916e70cda62d270e30a9a3
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2025-01-17 15:55:05 +0100

    feat: Working server implementation, do not use echo

--- cmd/chatserver/main.go
+++ cmd/chatserver/main.go
@@ -1,20 +1,18 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"os"
 
 	"github.com/KnoblauchPilze/backend-toolkit/pkg/config"
 	"github.com/KnoblauchPilze/backend-toolkit/pkg/logger"
-	"github.com/KnoblauchPilze/backend-toolkit/pkg/rest"
 	"github.com/Knoblauchpilze/chat-server/cmd/chatserver/internal"
 	"github.com/Knoblauchpilze/chat-server/pkg/tcp"
-	"github.com/labstack/echo/v4"
 )
 
 func determineConfigName() string {
 	if len(os.Args) < 2 {
-		return "users-prod.yml"
+		return "config-prod.yml"
 	}
 
 	return os.Args[1]
@@ -29,20 +27,11 @@ func main() {
 		os.Exit(1)
 	}
 
-	// https://github.com/labstack/echo?tab=readme-ov-file#example
-	e := echo.New()
-	e.HideBanner = true
-	e.HidePort = true
+	s := tcp.NewServer(conf.Server, log)
 
-	route := rest.ConcatenateEndpoints("/", conf.Server.BasePath)
-	e.GET(route, tcp.NewHandler(log))
-
-	log.Infof("Starting server at \"%v\" on port %v", route, conf.Server.Port)
-
-	address := fmt.Sprintf(":%d", conf.Server.Port)
-	err = e.Start(address)
+	err = s.Start(context.Background())
 	if err != nil {
-		log.Errorf("Failure while serving TCP: %v", err)
+		log.Errorf("Error while serving TCP: %v", err)
 		os.Exit(1)
 	}
 }
