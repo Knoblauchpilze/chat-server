319cee89c52dce5aabbe0d7abf8a1fc3eedba7f3
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2025-01-17 15:21:33 +0100

    fix: Wrong syntax for route and TCP connection handler

--- pkg/tcp/tcp_connection_handler.go
+++ pkg/tcp/tcp_connection_handler.go
@@ -5,9 +5,22 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+type tcpConnectionHandler struct {
+	log logger.Logger
+}
+
 func NewHandler(log logger.Logger) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		log.Infof("Received connection")
-		return nil
+	h := tcpConnectionHandler{
+		log: log,
 	}
+
+	return func(c echo.Context) error {
+		return h.HandleRequest(c)
+	}
+}
+
+func (h *tcpConnectionHandler) HandleRequest(c echo.Context) error {
+	// https://github.com/venilnoronha/tcp-echo-server/blob/master/main.go#L43
+	h.log.Infof("Received connection")
+	return nil
 }
