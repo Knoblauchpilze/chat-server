a903ab95d81b92fef7fceca28cc946d19e7cb5bd
KnoblauchPilze <prolific-programmer@tuta.com>
2025-01-24 18:45:42 +0100

    fix: Do not comment the fix

--- pkg/tcp/server.go
+++ pkg/tcp/server.go
@@ -117,8 +117,8 @@ func (s *serverImpl) shutdown() error {
 func (s *serverImpl) closeAllConnections() {
 	// Copy all connections to prevent dead locks in case one is
 	// removed due to a disconnect or read error.
-	var allConnections map[uuid.UUID]ConnectionListener
-	// allConnections := make(map[uuid.UUID]ConnectionListener)
+	// var allConnections map[uuid.UUID]ConnectionListener
+	allConnections := make(map[uuid.UUID]ConnectionListener)
 
 	fmt.Printf("Starting closing, copying connections\n")
 	func() {
