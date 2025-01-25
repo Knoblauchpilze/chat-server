51aba2ac56f5b91b7f53d73bf00991cc68bbdb5f
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2025-01-25 11:45:53 +0100

    feat: Updated database structure to have user and room ban

--- database/chats/migrations/100_seed_data.down.sql
+++ database/chats/migrations/100_seed_data.down.sql
@@ -1,8 +1,9 @@
 
 DELETE FROM message;
 
-DELETE FROM room_user;
+DELETE FROM room_ban;
 DELETE FROM user_ban;
+DELETE FROM room_user;
 DELETE FROM room;
 
 DELETE FROM user_status;
--- database/chats/migrations/3_create_rooms.down.sql
+++ database/chats/migrations/3_create_rooms.down.sql
@@ -1,7 +1,9 @@
 
+DROP TRIGGER trigger_room_ban_updated_at ON room_ban;
 DROP TRIGGER trigger_user_ban_updated_at ON user_ban;
 DROP TRIGGER trigger_room_updated_at ON room;
 
+DROP TABLE room_ban;
 DROP TABLE user_ban;
 DROP TABLE room_user;
 DROP TABLE room;
