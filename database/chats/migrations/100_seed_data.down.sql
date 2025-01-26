b43877b21b9f9d6f8a71e2f7fe2ed82ef638bd37
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2025-01-26 12:50:46 +0100

    feat: We don't need the user_status table

--- database/chats/migrations/100_seed_data.down.sql
+++ database/chats/migrations/100_seed_data.down.sql
@@ -6,5 +6,4 @@ DELETE FROM user_ban;
 DELETE FROM room_user;
 DELETE FROM room;
 
-DELETE FROM user_status;
 DELETE FROM chat_user;
--- database/chats/migrations/2_create_chat_users.down.sql
+++ database/chats/migrations/2_create_chat_users.down.sql
@@ -1,6 +1,4 @@
 
-DROP TRIGGER trigger_user_status_updated_at ON user_status;
 DROP TRIGGER trigger_chat_user_updated_at ON chat_user;
 
-DROP TABLE user_status;
 DROP TABLE chat_user;
