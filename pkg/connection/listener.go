315e54abc782ba1ffa0e0f4eb383fba6ecfd2a19
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2025-02-05 16:42:25 +0100

    feat: Implemented connection listener

---
+++ pkg/connection/listener.go
@@ -0,0 +1,92 @@
+package connection
+
+import (
+	"net"
+	"time"
+
+	"github.com/Knoblauchpilze/chat-server/pkg/errors"
+	"github.com/google/uuid"
+)
+
+type ListenerOptions struct {
+	ReadTimeout time.Duration
+	Callbacks   Callbacks
+}
+
+type Listener interface {
+	Id() uuid.UUID
+	Start()
+	Close()
+}
+
+type listenerImpl struct {
+	id uuid.UUID
+
+	conn      connection
+	callbacks Callbacks
+
+	quit chan interface{}
+	done chan bool
+}
+
+func New(conn net.Conn, opts ListenerOptions) Listener {
+	connOpts := connectionOptions{
+		ReadTimeout: opts.ReadTimeout,
+	}
+
+	l := &listenerImpl{
+		id:        uuid.New(),
+		conn:      WithOptions(conn, connOpts),
+		callbacks: opts.Callbacks,
+		quit:      make(chan interface{}),
+		done:      make(chan bool),
+	}
+
+	return l
+}
+
+func (l *listenerImpl) Id() uuid.UUID {
+	return l.id
+}
+
+func (l *listenerImpl) Start() {
+	// https://github.com/venilnoronha/tcp-echo-server/blob/master/main.go#L43
+	go l.activeLoop()
+}
+
+func (l *listenerImpl) Close() {
+	close(l.quit)
+	<-l.done
+}
+
+func (l *listenerImpl) activeLoop() {
+	defer func() {
+		l.done <- true
+	}()
+
+	running := true
+	for running {
+		var timeout bool
+		var err error
+
+		readPanic := errors.SafeRunSync(func() {
+			timeout, err = readFromConnection(l.id, l.conn, l.callbacks)
+		})
+
+		if timeout {
+			select {
+			case <-l.quit:
+				running = false
+			default:
+			}
+		}
+
+		if readPanic != nil {
+			l.callbacks.OnReadError(l.id, readPanic)
+		}
+
+		if err != nil {
+			running = false
+		}
+	}
+}
---
+++ pkg/connection/listener_test.go
@@ -0,0 +1,132 @@
+package connection
+
+import (
+	"testing"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+const sampleReadTimeout = 500 * time.Millisecond
+
+var sampleListenerOptions = ListenerOptions{
+	ReadTimeout: sampleReadTimeout,
+}
+
+func TestUnit_Listener_StartStop(t *testing.T) {
+	client, _ := newTestConnection()
+	listener := New(client, sampleListenerOptions)
+
+	listener.Start()
+	listener.Close()
+}
+
+func TestUnit_Listener_WhenDataReceived_ExpectCallbackNotified(t *testing.T) {
+	client, server := newTestConnection()
+
+	var called int
+	var actualData []byte
+	opts := ListenerOptions{
+		ReadTimeout: sampleReadTimeout,
+		Callbacks: Callbacks{
+			ReadDataCallbacks: []OnReadData{
+				func(id uuid.UUID, data []byte) {
+					called++
+					actualData = data
+				},
+			},
+		},
+	}
+	listener := New(server, opts)
+
+	asyncWriteSampleDataToConnection(t, client)
+	listener.Start()
+	listener.Close()
+
+	assert.Equal(t, 1, called)
+	assert.Equal(t, sampleData, actualData)
+}
+
+func TestUnit_Listener_WhenClientDisconnects_ExpectCallbackNotified(t *testing.T) {
+	client, server := newTestConnection()
+
+	var called int
+	opts := ListenerOptions{
+		Callbacks: Callbacks{
+			DisconnectCallbacks: []OnDisconnect{
+				func(id uuid.UUID) {
+					called++
+				},
+			},
+		},
+	}
+	listener := New(server, opts)
+
+	client.Close()
+	listener.Start()
+	listener.Close()
+
+	assert.Equal(t, 1, called)
+}
+
+func TestUnit_Listener_WhenReadDataPanics_ExpectErrorCallbackNotified(t *testing.T) {
+	client, server := newTestConnection()
+
+	var called int
+	var actualErr error
+	opts := ListenerOptions{
+		ReadTimeout: sampleReadTimeout,
+		Callbacks: Callbacks{
+			ReadDataCallbacks: []OnReadData{
+				func(id uuid.UUID, data []byte) {
+					panic(errSample)
+				},
+			},
+			ReadErrorCallbacks: []OnReadError{
+				func(id uuid.UUID, err error) {
+					called++
+					actualErr = err
+				},
+			},
+		},
+	}
+	listener := New(server, opts)
+
+	asyncWriteSampleDataToConnection(t, client)
+	listener.Start()
+	listener.Close()
+
+	assert.Equal(t, 1, called)
+	assert.Equal(t, errSample, actualErr)
+}
+
+func TestUnit_Listener_WhenFirstReadTimeouts_ExpectDataCanStillBeRead(t *testing.T) {
+	client, server := newTestConnection()
+
+	var called int
+	var actualData []byte
+	opts := ListenerOptions{
+		ReadTimeout: 500 * time.Millisecond,
+		Callbacks: Callbacks{
+			ReadDataCallbacks: []OnReadData{
+				func(id uuid.UUID, data []byte) {
+					called++
+					actualData = data
+				},
+			},
+		},
+	}
+	listener := New(server, opts)
+
+	// Write after a longer delay than the read timeout
+	asyncWriteSampleDataToConnectionWithDelay(t, client, 750*time.Millisecond)
+	listener.Start()
+
+	// Wait long enough to close to allow the write to happen
+	time.Sleep(1 * time.Second)
+	listener.Close()
+
+	assert.Equal(t, 1, called)
+	assert.Equal(t, sampleData, actualData)
+}
