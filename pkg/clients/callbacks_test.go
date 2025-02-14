e5fb21939db8b8fb38300ead25368994012c1930
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2025-02-14 09:51:27 +0100

    feat: Added client callbacks

---
+++ pkg/clients/callbacks_test.go
@@ -0,0 +1,63 @@
+package clients
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+var errSample = fmt.Errorf("sample error")
+
+func TestUnit_Callbacks_OnConnect_WhenUnset_ExpectNoFatalFailure(t *testing.T) {
+	var callbacks Callbacks
+
+	callback := func() {
+		callbacks.OnConnect(uuid.New(), "127.0.0.1:47134")
+	}
+	assert.NotPanics(t, callback)
+}
+
+func TestUnit_Callbacks_OnConnect_WhenUnset_ExpectConnectionAccepted(t *testing.T) {
+	var callbacks Callbacks
+
+	actual := callbacks.OnConnect(uuid.New(), "127.0.0.1:47134")
+
+	assert.True(t, actual)
+}
+
+func TestUnit_Callbacks_OnDisonnect_WhenUnset_ExpectNoFatalFailure(t *testing.T) {
+	var callbacks Callbacks
+
+	callback := func() {
+		callbacks.OnDisconnect(uuid.New())
+	}
+	assert.NotPanics(t, callback)
+}
+
+func TestUnit_Callbacks_OnReadData_WhenUnset_ExpectNoFatalFailure(t *testing.T) {
+	var callbacks Callbacks
+
+	callback := func() {
+		callbacks.OnReadData(uuid.New(), []byte{})
+	}
+	assert.NotPanics(t, callback)
+}
+
+func TestUnit_Callbacks_OnReadData_WhenUnset_ExpectConnectionStaysOpen(t *testing.T) {
+	var callbacks Callbacks
+
+	actual := callbacks.OnReadData(uuid.New(), []byte{})
+
+	assert.True(t, actual)
+}
+
+func TestUnit_Callbacks_OnReadError_WhenUnset_ExpectNoFatalFailure(t *testing.T) {
+	var callbacks Callbacks
+
+	callback := func() {
+		callbacks.OnReadError(uuid.New(), errSample)
+	}
+	assert.NotPanics(t, callback)
+}
