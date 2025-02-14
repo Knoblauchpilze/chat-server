1359cd21c62204534d1d8d404a99439e3af36a5f
KnoblauchPilze <prolific-programmer@tuta.com>
2025-02-14 09:51:27 +0100

    feat: Added client callbacks

---
+++ pkg/clients/callbacks.go
@@ -0,0 +1,48 @@
+package clients
+
+import "github.com/google/uuid"
+
+// The return value should indicate whether or not the connection is accepted.
+type OnConnect func(id uuid.UUID, address string) bool
+
+// The connection will automatically be closed after this callback is triggered.
+type OnDisconnect func(id uuid.UUID)
+
+// The return value should indicate whether or not the connection should stay open.
+type OnReadData func(id uuid.UUID, data []byte) bool
+
+// The connection will automatically be closed after this callback is triggered.
+type OnReadError func(id uuid.UUID, err error)
+
+type Callbacks struct {
+	ConnectCallback    OnConnect
+	DisconnectCallback OnDisconnect
+	ReadErrorCallback  OnReadError
+	ReadDataCallback   OnReadData
+}
+
+func (c Callbacks) OnConnect(id uuid.UUID, address string) bool {
+	if c.ConnectCallback == nil {
+		return true
+	}
+	return c.ConnectCallback(id, address)
+}
+
+func (c Callbacks) OnDisconnect(id uuid.UUID) {
+	if c.DisconnectCallback != nil {
+		c.DisconnectCallback(id)
+	}
+}
+
+func (c Callbacks) OnReadData(id uuid.UUID, data []byte) bool {
+	if c.ReadDataCallback == nil {
+		return true
+	}
+	return c.ReadDataCallback(id, data)
+}
+
+func (c Callbacks) OnReadError(id uuid.UUID, err error) {
+	if c.ReadErrorCallback != nil {
+		c.ReadErrorCallback(id, err)
+	}
+}
