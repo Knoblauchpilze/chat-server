f22def764d4419daf61e5955be56e6c2fd2e0eed
KnoblauchPilze <prolific-programmer@tuta.com>
2025-02-14 16:26:40 +0100

    feat: Added read timeout as part of the server configuration

--- cmd/chat-server/internal/config_test.go
+++ cmd/chat-server/internal/config_test.go
@@ -2,12 +2,18 @@ package internal
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestUnit_DefaultConfig_DefinesCorrectPort(t *testing.T) {
+func TestUnit_DefaultConfig_UsesPort80(t *testing.T) {
 	config := DefaultConfig()
 
 	assert.Equal(t, uint16(80), config.Port)
+}
+func TestUnit_DefaultConfig_Defines3sReadTimeout(t *testing.T) {
+	config := DefaultConfig()
+
+	assert.Equal(t, 3*time.Second, config.ReadTimeout)
 }
