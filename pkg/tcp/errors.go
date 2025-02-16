0cdef9838255fd6c932736773e65d769332f4357
KnoblauchPilze <prolific-programmer@tuta.com>
2025-02-16 12:08:02 +0100

    feat: Added a server to connect both the acceptor and the manager

--- pkg/tcp/errors.go
+++ pkg/tcp/errors.go
@@ -5,4 +5,5 @@ import "github.com/Knoblauchpilze/backend-toolkit/pkg/errors"
 const (
 	ErrTcpInitialization errors.ErrorCode = 100
 	ErrAlreadyListening  errors.ErrorCode = 101
+	ErrAlreadyRunning    errors.ErrorCode = 102
 )
