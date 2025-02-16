e1b7b2421f095c3a4f2d39e9c0f7b3347f08c375
KnoblauchPilze <prolific-programmer@tuta.com>
2025-02-16 12:24:26 +0100

    feat: Updated server to use the new TCP server

--- cmd/chat-server/internal/config_test.go
+++ cmd/chat-server/internal/config_test.go
@@ -2,7 +2,6 @@ package internal
 
 import (
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -11,9 +10,4 @@ func TestUnit_DefaultConfig_UsesPort80(t *testing.T) {
 	config := DefaultConfig()
 
 	assert.Equal(t, uint16(80), config.Port)
-}
-func TestUnit_DefaultConfig_Defines3sReadTimeout(t *testing.T) {
-	config := DefaultConfig()
-
-	assert.Equal(t, 3*time.Second, config.ReadTimeout)
 }
