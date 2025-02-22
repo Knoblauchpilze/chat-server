063a30024a0b3914db5d4d4e213ded78728f1bf2
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2025-02-22 15:04:07 +0100

    feat: Forward the callbacks instead of redefining them

--- cmd/chat-server/internal/server.go
+++ cmd/chat-server/internal/server.go
@@ -4,29 +4,14 @@ import (
 	"context"
 
 	"github.com/Knoblauchpilze/backend-toolkit/pkg/logger"
-	"github.com/Knoblauchpilze/chat-server/pkg/clients"
 	"github.com/Knoblauchpilze/chat-server/pkg/tcp"
-	"github.com/google/uuid"
 )
 
 func ListenAndServe(ctx context.Context,
 	config Configuration, log logger.Logger) error {
 	tcpConfig := tcp.ServerConfiguration{
-		Port: config.Port,
-		Callbacks: clients.Callbacks{
-			ConnectCallback: func(id uuid.UUID, address string) bool {
-				return config.Callbacks.OnConnect(id, address)
-			},
-			DisconnectCallback: func(id uuid.UUID) {
-				config.Callbacks.OnDisconnect(id)
-			},
-			ReadErrorCallback: func(id uuid.UUID, err error) {
-				config.Callbacks.OnReadError(id, err)
-			},
-			ReadDataCallback: func(id uuid.UUID, data []byte) bool {
-				return config.Callbacks.OnReadData(id, data)
-			},
-		},
+		Port:      config.Port,
+		Callbacks: config.Callbacks,
 	}
 
 	s, err := tcp.NewServer(tcpConfig, log)
