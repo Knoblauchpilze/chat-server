43d85eb7939770f834f2eed1dff6734625b97b01
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2025-02-23 18:24:17 +0100

    fix: Updated default port to be something not reserved

--- cmd/chat-server/internal/config_test.go
+++ cmd/chat-server/internal/config_test.go
@@ -6,8 +6,17 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestUnit_DefaultConfig_UsesPort80(t *testing.T) {
+func TestUnit_DefaultConfig_UsesPort49152(t *testing.T) {
 	config := DefaultConfig()
 
-	assert.Equal(t, uint16(80), config.Port)
+	assert.Equal(t, uint16(49152), config.Port)
+}
+
+func TestUnit_DefaultConfig_DoesNotSetAnyCallbacks(t *testing.T) {
+	config := DefaultConfig()
+
+	assert.Nil(t, config.Callbacks.ConnectCallback)
+	assert.Nil(t, config.Callbacks.DisconnectCallback)
+	assert.Nil(t, config.Callbacks.ReadErrorCallback)
+	assert.Nil(t, config.Callbacks.ReadDataCallback)
 }
