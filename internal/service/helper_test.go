d5604b04f004635819580984e103685c50939fec
KnoblauchPilze <prolific-programmer@tuta.com>
2025-03-22 10:19:26 +0100

    feat: Added room service and tests

---
+++ internal/service/helper_test.go
@@ -0,0 +1,22 @@
+package service
+
+import (
+	"context"
+	"testing"
+
+	"github.com/Knoblauchpilze/backend-toolkit/pkg/db"
+	"github.com/Knoblauchpilze/backend-toolkit/pkg/db/postgresql"
+	"github.com/stretchr/testify/assert"
+)
+
+var dbTestConfig = postgresql.NewConfigForLocalhost(
+	"db_chat_server",
+	"chat_server_manager",
+	"manager_password",
+)
+
+func newTestConnection(t *testing.T) db.Connection {
+	conn, err := db.New(context.Background(), dbTestConfig)
+	assert.Nil(t, err, "Actual err: %v", err)
+	return conn
+}
---
+++ internal/service/room_service_test.go
@@ -0,0 +1,153 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/Knoblauchpilze/backend-toolkit/pkg/db"
+	"github.com/Knoblauchpilze/backend-toolkit/pkg/db/pgx"
+	"github.com/Knoblauchpilze/backend-toolkit/pkg/errors"
+	"github.com/Knoblauchpilze/chat-server/pkg/communication"
+	"github.com/Knoblauchpilze/chat-server/pkg/persistence"
+	"github.com/Knoblauchpilze/chat-server/pkg/repositories"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIT_RoomService_Create(t *testing.T) {
+	id := uuid.New()
+	roomDtoRequest := communication.RoomDtoRequest{
+		Name: fmt.Sprintf("my-room-%s", id),
+	}
+
+	service, conn := newTestRoomRepository(t)
+	out, err := service.Create(context.Background(), roomDtoRequest)
+
+	assert.Nil(t, err, "Actual err: %v", err)
+
+	assert.Equal(t, roomDtoRequest.Name, out.Name)
+	assertRoomExists(t, conn, out.Id)
+}
+
+func TestIT_RoomService_Create_InvalidName(t *testing.T) {
+	roomDtoRequest := communication.RoomDtoRequest{
+		Name: "",
+	}
+
+	service, _ := newTestRoomRepository(t)
+	_, err := service.Create(context.Background(), roomDtoRequest)
+
+	assert.True(
+		t,
+		errors.IsErrorWithCode(err, InvalidName),
+		"Actual err: %v",
+		err,
+	)
+}
+
+func TestIT_RoomService_Create_WhenRoomWithSameNameAlreadyExists_ExpectFailure(t *testing.T) {
+	service, conn := newTestRoomRepository(t)
+	room := insertTestRoom(t, conn)
+	roomDtoRequest := communication.RoomDtoRequest{
+		Name: room.Name,
+	}
+
+	_, err := service.Create(context.Background(), roomDtoRequest)
+
+	assert.True(t, errors.IsErrorWithCode(err, pgx.UniqueConstraintViolation), "Actual err: %v", err)
+}
+
+func TestIT_RoomService_Get(t *testing.T) {
+	service, conn := newTestRoomRepository(t)
+	room := insertTestRoom(t, conn)
+
+	actual, err := service.Get(context.Background(), room.Id)
+
+	assert.Nil(t, err, "Actual err: %v", err)
+	assert.Equal(t, room.Id, actual.Id)
+	assert.Equal(t, room.Name, actual.Name)
+}
+
+func TestIT_RoomService_Get_WhenRoomDoesNotExist_ExpectFailure(t *testing.T) {
+	nonExistingId := uuid.MustParse("00000000-0000-1221-0000-000000000000")
+
+	service, _ := newTestRoomRepository(t)
+	_, err := service.Get(context.Background(), nonExistingId)
+
+	assert.True(
+		t,
+		errors.IsErrorWithCode(err, db.NoMatchingRows),
+		"Actual err: %v",
+		err,
+	)
+}
+
+func TestIT_RoomService_Delete(t *testing.T) {
+	service, conn := newTestRoomRepository(t)
+	room := insertTestRoom(t, conn)
+
+	err := service.Delete(context.Background(), room.Id)
+
+	assert.Nil(t, err, "Actual err: %v", err)
+	assertRoomDoesNotExist(t, conn, room.Id)
+}
+
+func TestIT_RoomService_Delete_WhenRoomDoesNotExist_ExpectSuccess(t *testing.T) {
+	nonExistingId := uuid.MustParse("00000000-0000-1221-0000-000000000000")
+
+	service, _ := newTestRoomRepository(t)
+	err := service.Delete(context.Background(), nonExistingId)
+
+	assert.Nil(t, err, "Actual err: %v", err)
+}
+
+func newTestRoomRepository(t *testing.T) (RoomService, db.Connection) {
+	conn := newTestConnection(t)
+
+	repos := repositories.Repositories{
+		Room: repositories.NewRoomRepository(conn),
+	}
+
+	return NewRoomService(conn, repos), conn
+}
+
+func insertTestRoom(t *testing.T, conn db.Connection) persistence.Room {
+	repo := repositories.NewRoomRepository(conn)
+
+	id := uuid.New()
+	room := persistence.Room{
+		Id:        id,
+		Name:      fmt.Sprintf("my-room-%s", id),
+		CreatedAt: time.Now(),
+	}
+	out, err := repo.Create(context.Background(), room)
+	assert.Nil(t, err, "Actual err: %v", err)
+
+	assertRoomExists(t, conn, out.Id)
+
+	return out
+}
+
+func assertRoomExists(t *testing.T, conn db.Connection, id uuid.UUID) {
+	value, err := db.QueryOne[uuid.UUID](
+		context.Background(),
+		conn,
+		"SELECT id FROM room WHERE id = $1",
+		id,
+	)
+	assert.Nil(t, err, "Actual err: %v", err)
+	assert.Equal(t, id, value)
+}
+
+func assertRoomDoesNotExist(t *testing.T, conn db.Connection, id uuid.UUID) {
+	value, err := db.QueryOne[int](
+		context.Background(),
+		conn,
+		"SELECT COUNT(id) FROM room WHERE id = $1",
+		id,
+	)
+	assert.Nil(t, err, "Actual err: %v", err)
+	assert.Zero(t, value)
+}
