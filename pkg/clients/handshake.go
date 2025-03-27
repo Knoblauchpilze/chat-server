69d0a284ee245959e408796371a1472d687cec41
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2025-03-27 23:19:47 +0100

    feat: Simple handshake protocol to retrive an id from the connection

---
+++ pkg/clients/handshake.go
@@ -0,0 +1,33 @@
+package clients
+
+import (
+	"net"
+	"time"
+
+	"github.com/Knoblauchpilze/backend-toolkit/pkg/errors"
+	"github.com/google/uuid"
+)
+
+func Handshake(conn net.Conn, timeout time.Duration) (uuid.UUID, error) {
+	limit := time.Now().Add(timeout)
+	conn.SetReadDeadline(limit)
+
+	var id uuid.UUID
+	data := make([]byte, len(id))
+
+	n, err := conn.Read(data)
+	if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
+		return uuid.Nil, errors.NewCode(HandshakeTimeout)
+	} else if err != nil {
+		return uuid.Nil, errors.WrapCode(err, HandshakeFailure)
+	} else if n != len(id) {
+		return uuid.Nil, errors.NewCode(IncompleteHandshake)
+	}
+
+	id, err = uuid.FromBytes(data)
+	if err != nil {
+		return uuid.Nil, errors.WrapCode(err, HandshakeFailure)
+	}
+
+	return id, nil
+}
