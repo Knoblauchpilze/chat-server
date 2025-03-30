0d8b65e72de6cb717f288fd31030b10a01d3117b
KnoblauchPilze <prolific-programmer@tuta.com>
2025-03-30 16:55:50 +0200

    fix: Attempt to always return in UTC for rooms

--- pkg/repositories/room_repository.go
+++ pkg/repositories/room_repository.go
@@ -40,8 +40,11 @@ func (r *roomRepositoryImpl) Create(
 		room.Id,
 		room.Name,
 	)
-	room.CreatedAt = times.CreatedAt
-	room.UpdatedAt = times.UpdatedAt
+
+	// https://www.reddit.com/r/golang/comments/1gbvowf/dealing_with_timezone_issues_when_running_unit/
+	room.CreatedAt = times.CreatedAt.UTC()
+	room.UpdatedAt = times.UpdatedAt.UTC()
+
 	return room, err
 }
 
@@ -59,7 +62,14 @@ WHERE
 func (r *roomRepositoryImpl) Get(
 	ctx context.Context, id uuid.UUID,
 ) (persistence.Room, error) {
-	return db.QueryOne[persistence.Room](ctx, r.conn, getRoomSqlTemplate, id)
+	room, err := db.QueryOne[persistence.Room](ctx, r.conn, getRoomSqlTemplate, id)
+
+	if err == nil {
+		room.CreatedAt = room.CreatedAt.UTC()
+		room.UpdatedAt = room.UpdatedAt.UTC()
+	}
+
+	return room, err
 }
 
 const listForUserSqlTemplate = `
@@ -77,7 +87,16 @@ WHERE
 func (r *roomRepositoryImpl) ListForUser(
 	ctx context.Context, user uuid.UUID,
 ) ([]persistence.Room, error) {
-	return db.QueryAll[persistence.Room](ctx, r.conn, listForUserSqlTemplate, user)
+	rooms, err := db.QueryAll[persistence.Room](ctx, r.conn, listForUserSqlTemplate, user)
+
+	if err == nil {
+		for id, room := range rooms {
+			rooms[id].CreatedAt = room.CreatedAt.UTC()
+			rooms[id].UpdatedAt = room.UpdatedAt.UTC()
+		}
+	}
+
+	return rooms, err
 }
 
 const deleteRoomSqlTemplate = `
