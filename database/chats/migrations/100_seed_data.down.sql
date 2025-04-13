1e90f5368037701b88bb0bf2596346868e2c1838
KnoblauchPilze <prolific-programmer@tuta.com>
2025-04-13 09:32:09 +0200

    feat: Improved DB seeding and created general room

--- database/chats/migrations/100_seed_data.down.sql
+++ database/chats/migrations/100_seed_data.down.sql
@@ -1,9 +1,7 @@
 
-DELETE FROM message;
-
-DELETE FROM room_ban;
-DELETE FROM user_ban;
-DELETE FROM room_user;
-DELETE FROM room;
-
-DELETE FROM chat_user;
+DELETE FROM message WHERE id = 'd0080da7-e95a-4ba3-ad0d-78ed72688e2e';
+DELETE FROM room_user WHERE
+  room = 'b2c0d9c8-c5bd-42ea-88e2-15b66fffdd68'
+  AND chat_user = '0edebba5-fd0b-433d-bb6b-35ceb9fcb9b3';
+DELETE FROM chat_user WHERE id = '0edebba5-fd0b-433d-bb6b-35ceb9fcb9b3';
+DELETE FROM room WHERE id = 'b2c0d9c8-c5bd-42ea-88e2-15b66fffdd68';
---
+++ database/chats/migrations/101_seed_test_data.down.sql
@@ -0,0 +1,9 @@
+
+DELETE FROM message;
+
+DELETE FROM room_ban;
+DELETE FROM user_ban;
+DELETE FROM room_user;
+DELETE FROM room;
+
+DELETE FROM chat_user;
