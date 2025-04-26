f7c96f582c1c9c4d8291650dfa093e5502d6a12b
KnoblauchPilze <prolific-programmer@tuta.com>
2025-04-26 18:15:58 +0200

    chore: Prefixed error codes with Err in clients package

--- pkg/clients/errors.go
+++ pkg/clients/errors.go
@@ -3,22 +3,22 @@ package clients
 import "github.com/Knoblauchpilze/backend-toolkit/pkg/errors"
 
 const (
-	HandshakeTimeout    errors.ErrorCode = 500
-	IncompleteHandshake errors.ErrorCode = 501
-	HandshakeFailure    errors.ErrorCode = 502
+	ErrHandshakeTimeout    errors.ErrorCode = 500
+	ErrIncompleteHandshake errors.ErrorCode = 501
+	ErrHandshakeFailure    errors.ErrorCode = 502
 )
 
 func newHandshakeTimeoutError() error {
-	return errors.NewCodeWithDetails(HandshakeTimeout, "timeout")
+	return errors.NewCodeWithDetails(ErrHandshakeTimeout, "timeout")
 }
 
 func newHandshakeIncompleteError() error {
 	return errors.NewCodeWithDetails(
-		IncompleteHandshake,
+		ErrIncompleteHandshake,
 		"not enough data to received to perform handshake",
 	)
 }
 
 func wrapHandshakeFailureError(err error) error {
-	return errors.WrapCode(err, HandshakeFailure)
+	return errors.WrapCode(err, ErrHandshakeFailure)
 }
