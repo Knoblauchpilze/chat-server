3fafdaee80563095ca2339cfcec2a121a1444ec4
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2025-05-04 21:17:08 +0200

    feat: Tests for Client interface

--- pkg/clients/errors.go
+++ pkg/clients/errors.go
@@ -7,8 +7,9 @@ const (
 	ErrIncompleteHandshake errors.ErrorCode = 501
 	ErrHandshakeFailure    errors.ErrorCode = 502
 
-	ErrPartialSseWrite errors.ErrorCode = 503
-	ErrSseStreamFailed errors.ErrorCode = 504
+	ErrPartialSseWrite       errors.ErrorCode = 503
+	ErrSseStreamFailed       errors.ErrorCode = 504
+	ErrUnsupportedConnection errors.ErrorCode = 505
 )
 
 func newHandshakeTimeoutError() error {
