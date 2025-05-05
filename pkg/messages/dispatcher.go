d4c82274b9e2a80cecf2be18253f27f01abd1cfd
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2025-05-05 20:44:10 +0200

    chore: TODO statement

--- pkg/messages/dispatcher.go
+++ pkg/messages/dispatcher.go
@@ -3,6 +3,7 @@ package messages
 import "github.com/google/uuid"
 
 type Dispatcher interface {
+	// TODO: This should be using communication.MessageResponseDto
 	Broadcast(msg Message)
 	BroadcastExcept(id uuid.UUID, msg Message)
 	SendTo(id uuid.UUID, msg Message)
