7bea84a3ce78d8a8aa7b3945bf403357c194627f
KnoblauchPilze <prolific-programmer@tuta.com>
2025-05-17 10:33:34 +0200

    feat: Added registration repository

---
+++ pkg/repositories/registration_repository.go
@@ -0,0 +1,77 @@
+package repositories
+
+import (
+	"context"
+
+	"github.com/Knoblauchpilze/backend-toolkit/pkg/db"
+	"github.com/Knoblauchpilze/backend-toolkit/pkg/errors"
+	"github.com/google/uuid"
+)
+
+type RegistrationRepository interface {
+	RegisterInRoom(ctx context.Context, tx db.Transaction, user uuid.UUID, room uuid.UUID) error
+	RegisterInRoomByName(ctx context.Context, tx db.Transaction, user uuid.UUID, room string) error
+	RegisterByNameInRoom(ctx context.Context, tx db.Transaction, user string, room uuid.UUID) error
+}
+
+type registrationRepositoryImpl struct{}
+
+func NewRegistrationRepository() RegistrationRepository {
+	return &registrationRepositoryImpl{}
+}
+
+const registerInRoomSqlTemplate = `
+INSERT INTO room_user (chat_user, room)
+	VALUES ($1, $2)
+	RETURNING created_at`
+
+func (r *registrationRepositoryImpl) RegisterInRoom(
+	ctx context.Context, tx db.Transaction, user uuid.UUID, room uuid.UUID,
+) error {
+	_, err := tx.Exec(ctx, registerInRoomSqlTemplate, user, room)
+	return handleRegistrationError(err)
+}
+
+const registerInRoomByNameSqlTemplate = `
+INSERT INTO
+	room_user (chat_user, room)
+SELECT
+	$1,
+	id
+FROM
+	room
+WHERE
+	name = $2`
+
+func (r *registrationRepositoryImpl) RegisterInRoomByName(
+	ctx context.Context, tx db.Transaction, user uuid.UUID, room string,
+) error {
+	inserted, err := tx.Exec(ctx, registerInRoomByNameSqlTemplate, user, room)
+
+	if err == nil && inserted == 0 {
+		return errors.WrapCode(err, ErrNoSuchRoom)
+	}
+	return handleRegistrationError(err)
+}
+
+const registerByNameInRoomSqlTemplate = `
+INSERT INTO
+	room_user (chat_user, room)
+SELECT
+	id,
+	$2
+FROM
+	chat_user
+WHERE
+	name = $1`
+
+func (r *registrationRepositoryImpl) RegisterByNameInRoom(
+	ctx context.Context, tx db.Transaction, user string, room uuid.UUID,
+) error {
+	inserted, err := tx.Exec(ctx, registerByNameInRoomSqlTemplate, user, room)
+
+	if err == nil && inserted == 0 {
+		return errors.WrapCode(err, ErrNoSuchUser)
+	}
+	return handleRegistrationError(err)
+}
---
+++ pkg/repositories/registration_repository_test.go
@@ -0,0 +1,212 @@
+package repositories
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/Knoblauchpilze/backend-toolkit/pkg/db"
+	"github.com/Knoblauchpilze/backend-toolkit/pkg/errors"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIT_RegistrationRepository_RegisterUserInRoom(t *testing.T) {
+	repo, conn, tx := newTestRegistrationRepositoryAndTransaction(t)
+	defer conn.Close(context.Background())
+	user := insertTestUser(t, conn)
+	room := insertTestRoom(t, conn)
+
+	err := repo.RegisterInRoom(context.Background(), tx, user.Id, room.Id)
+	tx.Close(context.Background())
+	assert.Nil(t, err, "Actual err: %v", err)
+
+	assertUserRegisteredInRoom(t, conn, user.Id, room.Id)
+}
+
+func TestIT_RegistrationRepository_RegisterUserInRoom_WhenRoomDoesNotExist_ExpectFailure(t *testing.T) {
+	repo, conn, tx := newTestRegistrationRepositoryAndTransaction(t)
+	defer conn.Close(context.Background())
+	user := insertTestUser(t, conn)
+	room := uuid.New()
+
+	err := repo.RegisterInRoom(context.Background(), tx, user.Id, room)
+	tx.Close(context.Background())
+	assert.True(
+		t,
+		errors.IsErrorWithCode(err, ErrNoSuchRoom),
+		"Actual err: %v",
+		err,
+	)
+
+	assertUserNotRegisteredInRoom(t, conn, user.Id, room)
+}
+
+func TestIT_RegistrationRepository_RegisterUserInRoom_WhenUserDoesNotExist_ExpectFailure(t *testing.T) {
+	repo, conn, tx := newTestRegistrationRepositoryAndTransaction(t)
+	defer conn.Close(context.Background())
+	user := uuid.New()
+	room := insertTestRoom(t, conn)
+
+	err := repo.RegisterInRoom(context.Background(), tx, user, room.Id)
+	tx.Close(context.Background())
+	assert.True(
+		t,
+		errors.IsErrorWithCode(err, ErrNoSuchUser),
+		"Actual err: %v",
+		err,
+	)
+
+	assertUserNotRegisteredInRoom(t, conn, user, room.Id)
+}
+
+func TestIT_RegistrationRepository_RegisterUserInRoom_WhenUserAlreadyRegistered_ExpectFailure(t *testing.T) {
+	repo, conn, tx := newTestRegistrationRepositoryAndTransaction(t)
+	defer conn.Close(context.Background())
+	user := insertTestUser(t, conn)
+	room := insertTestRoom(t, conn)
+	registerUserInRoom(t, conn, user.Id, room.Id)
+
+	err := repo.RegisterInRoom(context.Background(), tx, user.Id, room.Id)
+	tx.Close(context.Background())
+	assert.True(
+		t,
+		errors.IsErrorWithCode(err, ErrUserAlreadyRegisteredInRoom),
+		"Actual err: %v",
+		err,
+	)
+}
+
+func TestIT_RegistrationRepository_RegisterUserInRoomByName(t *testing.T) {
+	repo, conn, tx := newTestRegistrationRepositoryAndTransaction(t)
+	defer conn.Close(context.Background())
+	user := insertTestUser(t, conn)
+	room := insertTestRoom(t, conn)
+
+	err := repo.RegisterInRoomByName(context.Background(), tx, user.Id, room.Name)
+	tx.Close(context.Background())
+	assert.Nil(t, err, "Actual err: %v", err)
+
+	assertUserRegisteredInRoom(t, conn, user.Id, room.Id)
+}
+
+func TestIT_RegistrationRepository_RegisterUserInRoomByName_WhenRoomDoesNotExist_ExpectFailure(t *testing.T) {
+	repo, conn, tx := newTestRegistrationRepositoryAndTransaction(t)
+	defer conn.Close(context.Background())
+	user := insertTestUser(t, conn)
+	room := fmt.Sprintf("my-inexistent-room-%s", uuid.New().String())
+
+	err := repo.RegisterInRoomByName(context.Background(), tx, user.Id, room)
+	tx.Close(context.Background())
+	assert.True(
+		t,
+		errors.IsErrorWithCode(err, ErrNoSuchRoom),
+		"Actual err: %v",
+		err,
+	)
+}
+
+func TestIT_RegistrationRepository_RegisterUserInRoomByName_WhenUserDoesNotExist_ExpectFailure(t *testing.T) {
+	repo, conn, tx := newTestRegistrationRepositoryAndTransaction(t)
+	defer conn.Close(context.Background())
+	user := uuid.New()
+	room := insertTestRoom(t, conn)
+
+	err := repo.RegisterInRoomByName(context.Background(), tx, user, room.Name)
+	tx.Close(context.Background())
+	assert.True(
+		t,
+		errors.IsErrorWithCode(err, ErrNoSuchUser),
+		"Actual err: %v",
+		err,
+	)
+
+	assertUserNotRegisteredInRoom(t, conn, user, room.Id)
+}
+
+func TestIT_RegistrationRepository_RegisterUserInRoomByName_WhenUserAlreadyRegistered_ExpectFailure(t *testing.T) {
+	repo, conn, tx := newTestRegistrationRepositoryAndTransaction(t)
+	defer conn.Close(context.Background())
+	user := insertTestUser(t, conn)
+	room := insertTestRoom(t, conn)
+	registerUserInRoom(t, conn, user.Id, room.Id)
+
+	err := repo.RegisterInRoomByName(context.Background(), tx, user.Id, room.Name)
+	tx.Close(context.Background())
+	assert.True(
+		t,
+		errors.IsErrorWithCode(err, ErrUserAlreadyRegisteredInRoom),
+		"Actual err: %v",
+		err,
+	)
+}
+
+func TestIT_RegistrationRepository_RegisterUserByNameInRoom(t *testing.T) {
+	repo, conn, tx := newTestRegistrationRepositoryAndTransaction(t)
+	defer conn.Close(context.Background())
+	user := insertTestUser(t, conn)
+	room := insertTestRoom(t, conn)
+
+	err := repo.RegisterByNameInRoom(context.Background(), tx, user.Name, room.Id)
+	tx.Close(context.Background())
+	assert.Nil(t, err, "Actual err: %v", err)
+
+	assertUserRegisteredInRoom(t, conn, user.Id, room.Id)
+}
+
+func TestIT_RegistrationRepository_RegisterUserByNameInRoom_WhenRoomDoesNotExist_ExpectFailure(t *testing.T) {
+	repo, conn, tx := newTestRegistrationRepositoryAndTransaction(t)
+	defer conn.Close(context.Background())
+	user := insertTestUser(t, conn)
+	room := uuid.New()
+
+	err := repo.RegisterByNameInRoom(context.Background(), tx, user.Name, room)
+	tx.Close(context.Background())
+	assert.True(
+		t,
+		errors.IsErrorWithCode(err, ErrNoSuchRoom),
+		"Actual err: %v",
+		err,
+	)
+	assertUserNotRegisteredInRoom(t, conn, user.Id, room)
+}
+
+func TestIT_RegistrationRepository_RegisterUserByNameInRoom_WhenUserDoesNotExist_ExpectFailure(t *testing.T) {
+	repo, conn, tx := newTestRegistrationRepositoryAndTransaction(t)
+	defer conn.Close(context.Background())
+	user := fmt.Sprintf("my-inexistent-user-%s", uuid.New().String())
+	room := insertTestRoom(t, conn)
+
+	err := repo.RegisterByNameInRoom(context.Background(), tx, user, room.Id)
+	tx.Close(context.Background())
+	assert.True(
+		t,
+		errors.IsErrorWithCode(err, ErrNoSuchUser),
+		"Actual err: %v",
+		err,
+	)
+}
+
+func TestIT_RegistrationRepository_RegisterByNameUserInRoom_WhenUserAlreadyRegistered_ExpectFailure(t *testing.T) {
+	repo, conn, tx := newTestRegistrationRepositoryAndTransaction(t)
+	defer conn.Close(context.Background())
+	user := insertTestUser(t, conn)
+	room := insertTestRoom(t, conn)
+	registerUserInRoom(t, conn, user.Id, room.Id)
+
+	err := repo.RegisterByNameInRoom(context.Background(), tx, user.Name, room.Id)
+	tx.Close(context.Background())
+	assert.True(
+		t,
+		errors.IsErrorWithCode(err, ErrUserAlreadyRegisteredInRoom),
+		"Actual err: %v",
+		err,
+	)
+}
+
+func newTestRegistrationRepositoryAndTransaction(t *testing.T) (RegistrationRepository, db.Connection, db.Transaction) {
+	conn := newTestConnection(t)
+	tx, err := conn.BeginTx(context.Background())
+	assert.Nil(t, err, "Actual err: %v", err)
+	return NewRegistrationRepository(), conn, tx
+}
